c62288a4e79a6dc3e5befabe4a2d9218bde214d6
Leocendino Cardoso <ncardosoleo@gmail.com>
2022-07-21 16:08:42 -0300

    Organização das pastas e manutenção dos nomes

---
+++ class/DetalheVenda.php
@@ -0,0 +1,28 @@
+<?php
+
+class DetalheVenda{
+    private $codVenda;
+    private $codProduto;
+    private $qtdProduto;
+
+    public function exist($argCodVenda, $argCodProduto){
+
+    }
+    public function add(){
+
+    }
+    public function getDetalheVenda($argCodVenda, $argCodProduto){
+
+    }
+    public function save(){
+
+    }
+    public function delete(){
+
+    }
+    public function getSubTotal(){
+
+    }
+    public function getProductList($argCodVenda)
+
+}
---
+++ conexaobd/ConexaoBD.php
@@ -0,0 +1,25 @@
+<?php
+
+class ConexaoBD
+{
+
+
+    public function execute()
+    {
+    }
+    public function query()
+    {
+    }
+    public function logicoSql()
+    {
+    }
+    public function pontoVirgula()
+    {
+    }
+    public function dataSql()
+    {
+    }
+    public function valorSql()
+    {
+    }
+}
