8e3db168fbcfa00a3734782751d7949e2b1abed3
an1car <109595625+an1car@users.noreply.github.com>
2022-08-16 21:48:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html style="font-size: 16px;" lang="pt">
-
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta charset="utf-8">
-  <meta name="keywords" content="LOJA PHP">
-  <meta name="description" content="">
-  <title>LOJA PHP</title>
-  <link rel="stylesheet" href="css/nicepage.css" media="screen">
-  <link rel="stylesheet" href="css/LOJA-PHP.css" media="screen">
+<html style="font-size: 16px;" lang="pt"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="LOJA PHP">
+    <meta name="description" content="">
+    <title>LOJA PHP</title>
+    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+	<link rel="stylesheet" href="css/LOJA-PHP.css" media="screen">
+    <script class="u-<head>
   <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
   <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
 
@@ -35,53 +34,21 @@
       <a class="u-image u-logo u-image-1" data-image-width="1280" data-image-height="1262">
         <img src="img/f69aed53dbb5bcd6f5cc6d7a9c8dda957767ea33ca1c67ac86ad20100f2d5b9e8a075b298c3e3dfac3accdc9edd9c5b19148fad85eb84492668394_1280.png" class="u-logo-image u-logo-image-1">
       </a>
-      <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
-        <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
-          <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
-            <svg class="u-svg-link" viewBox="0 0 24 24">
-              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use>
-            </svg>
-            <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-              <g>
-                <rect y="1" width="16" height="2"></rect>
-                <rect y="7" width="16" height="2"></rect>
-                <rect y="13" width="16" height="2"></rect>
-              </g>
-            </svg>
-          </a>
-        </div>
+      <div class="u-list u-list-1">
         <div class="u-custom-menu u-nav-container">
           <ul class="u-nav u-unstyled u-nav-1">
             <li class="u-nav-item"><a class="u-nav-link u-text-active-black" href="index.php" style="padding: 10px 20px;">LOJA PHP</a>
 	    </li>
-            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-hover-palette-2-base" href="produtos.php" style="padding: 10px 20px;">PRODUTOS</a>
+            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-hover-palette-2-base" href="pages/produtos.php" style="padding: 10px 20px;">PRODUTOS</a>
             </li>
-            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-hover-palette-2-base" href="VENDAS.php" style="padding: 10px 20px;">VENDAS</a>
+            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-hover-palette-2-base" href="pages/VENDAS.php" style="padding: 10px 20px;">VENDAS</a>
             </li>
-            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black" href="clientes.php" style="padding: 10px 20px;">CLIENTES</a>
+            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black" href="pages/clientes.php" style="padding: 10px 20px;">CLIENTES</a>
             </li>
           </ul>
         </div>
-        <div class="u-custom-menu u-nav-container-collapse">
-          <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
-            <div class="u-inner-container-layout u-sidenav-overflow">
-              <div class="u-menu-close"></div>
-              <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
-                <li><a href="index.php">LOJA PHP</a>
-                </li>
-                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="produtos.php">PRODUTOS</a>
-                </li>
-                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="VENDAS.php">VENDAS</a>
-                </li>
-                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="clientes.php">CLIENTES</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
-        </div>
-      </nav>
-    </div>
+	</div>
+       
   </header>
   <section class="u-align-left u-clearfix u-image u-section-1" id="carousel_fc6f" data-image-width="1920" data-image-height="960">
     <div class="u-clearfix u-sheet u-sheet-1">
@@ -92,7 +59,7 @@
               <h5 class="u-custom-font u-font-raleway u-text u-text-1">PRODUTOS</h5>
               <p class="u-text u-text-2">&gt; Lista ​​de produtos disponíveis<br>&gt; Cadastrar Produtos
               </p>
-              <a href="produtos.php" class="u-active-none u-border-2 u-border-active-black u-border-hover-black u-border-palette-1-base u-btn u-button-style u-hover-none u-none u-text-body-color u-text-hover-palette-1-base u-btn-1">acessar produtos</a>
+              <a href="pages/produtos.php" class="u-active-none u-border-2 u-border-active-black u-border-hover-black u-border-palette-1-base u-btn u-button-style u-hover-none u-none u-text-body-color u-text-hover-palette-1-base u-btn-1">acessar produtos</a>
             </div>
           </div>
           <div class="u-align-center u-container-style u-grey-5 u-list-item u-radius-15 u-repeater-item u-shape-round u-list-item-2">
@@ -100,7 +67,7 @@
               <h5 class="u-custom-font u-font-raleway u-text u-text-3">VENDAS</h5>
               <p class="u-text u-text-4">&gt; Lista de vendas<br>&gt; Detalhamento de vendas<br>&gt; Cadastrar vendas
               </p>
-              <a href="vendas.php" class="u-active-none u-border-2 u-border-active-black u-border-hover-black u-border-palette-1-base u-btn u-button-style u-hover-none u-none u-text-body-color u-text-hover-palette-1-base u-btn-2">ACESSAR VENDAS</a>
+              <a href="pages/vendas.php" class="u-active-none u-border-2 u-border-active-black u-border-hover-black u-border-palette-1-base u-btn u-button-style u-hover-none u-none u-text-body-color u-text-hover-palette-1-base u-btn-2">ACESSAR VENDAS</a>
             </div>
           </div>
           <div class="u-align-center u-container-style u-grey-5 u-list-item u-radius-15 u-repeater-item u-shape-round u-list-item-3">
@@ -108,7 +75,7 @@
               <h5 class="u-custom-font u-font-raleway u-text u-text-5">CLIENTES</h5>
               <p class="u-text u-text-6">&gt; Lista de Clientes<br>&gt; Cadastrar clientes
               </p>
-              <a href="clientes.php" class="u-active-none u-border-2 u-border-active-black u-border-hover-black u-border-palette-1-base u-btn u-button-style u-hover-none u-none u-text-black u-text-hover-palette-1-base u-btn-3">acessar clientes</a>
+              <a href="pages/clientes.php" class="u-active-none u-border-2 u-border-active-black u-border-hover-black u-border-palette-1-base u-btn u-button-style u-hover-none u-none u-text-black u-text-hover-palette-1-base u-btn-3">acessar clientes</a>
             </div>
           </div>
         </div>
